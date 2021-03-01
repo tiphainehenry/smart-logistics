b2d3fe2860002069ac2149ba77e20ffbc4e917bc
Tiphaine HENRY <tiphaine.henry@orange.com>
2021-03-01 12:11:56 +0100

    Update Homepage.js

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -42,7 +42,9 @@ class Homepage extends Component {
         <section className="jumbotron text-center">
             <div className="container">
             <h1>Marketplace 3.0</h1>
-            <p className="lead text-muted">Need a trusted platform to find the best service delivery assets? Try it the decentralized way. This webapp builds on the blockchain technology to manage a set of resources. Its services span from autonomous resource matching to smart contractualization and tamper-proof feedbacks.</p>
+            <p className="lead text-muted">Need a trusted mechanism to find the best service delivery driver? Try it the decentralized way. This webapp builds on the blockchain technology to manage a set of resources. Its services span from autonomous resource matching (see the "Search and hire" pannel) to SC-based binding management (see the "Manage Contracts" pannel).
+            Watch out: the resource availability has been tuned for January 12th, and January 13th 2021.
+      </p>
             <p>
                 <a href="/request" className="btn btn-primary my-2">Search and Hire</a>
                 <a href="/ecmr" className="btn btn-secondary my-2">Manage e-CMRs</a>
@@ -51,8 +53,12 @@ class Homepage extends Component {
         </section>
 
         <div className="album py-5 bg-light">
-            <div className="container">            
+            <div className="container">  
+               <div className="row">
+                 The below resource profiles are registered into the binding smart contract. Among the registered criteria figure their availability, their location (lat, long), and their truck equipement. Their experience and delay QoS ratings are also stored in the smart contract. The latter can update them upon each service fulfillment.
+               </div>  
                 <div className="row">
+
                     {this.state.resourceArray.map(i=> <ResourceId resource={resources[i]} hire={false}/>)}
                 </div>
             </div>
